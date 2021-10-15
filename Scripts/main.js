4d5cf7746136e1d75b058810abc6e49ca59e3c0c
jnordqvist <73642406+jnordqvist@users.noreply.github.com>
2021-10-15 09:31:22 +0200

    js started

---
+++ Scripts/main.js
@@ -0,0 +1,60 @@
+let searching = false
+const searchClicked = ()=>{
+    let container = document.querySelector("#searchContainer")
+    container.id = 'searchContainerExpanded'
+    document.querySelector("#search").src = "Resources/Images/close.svg"
+    console.log('hello2')
+}
+
+function openSearch(){
+    let container = document.querySelector("#searchContainer")
+    container.id = 'searchContainerExpanded'
+    document.querySelector("#search").src = "Resources/Images/close.svg"
+    document.querySelector("#searchField").focus()
+    searching = true
+}
+
+function closeSearch(){
+    let container = document.querySelector("#searchContainerExpanded")
+    container.id = 'searchContainer'
+    document.querySelector("#search").src = "Resources/Images/magnifyingGlass.svg"
+    searching = false
+}
+
+function toggleSearch(){
+    if (searching) {
+        return closeSearch()
+    }
+    else{
+        return openSearch()
+    }
+}
+
+document.querySelector("#search").addEventListener('click', toggleSearch)
+
+
+let burgerMenuOpen = false
+const slideBurgerOut = ()=>{
+    let menu = document.querySelector("header")
+    menu.style.height = "200px"
+    document.querySelector("#burger").src = "Resources/Images/burgerOpen.svg"
+    burgerMenuOpen = true;
+}
+
+const slideBurgerIn = ()=>{
+    let menu = document.querySelector("header")
+    menu.style.height = "50px"
+    document.querySelector("#burger").src = "Resources/Images/burgerClosed.svg"
+    burgerMenuOpen = false;
+}
+
+const toggleBurger = () =>{
+    if (burgerMenuOpen) {
+        return slideBurgerIn()
+    }
+    else{
+        return slideBurgerOut()
+    }
+}
+
+document.querySelector("#burger").addEventListener('click', toggleBurger)
